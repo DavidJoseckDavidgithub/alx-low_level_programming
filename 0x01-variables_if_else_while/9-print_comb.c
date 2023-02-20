bcd2b5b815c69b76184f80cb475bda9c9f55b56a
DavidJoseckDavidgithub <joseckonyinodavid@gmail.com>
2023-02-19 23:31:14 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,14 +4,20 @@
  *
  * Return: Always 0 (success)
  */
+
 int main(void)
 {
-	int ch;
+	int a;
 
-	for (ch = 'a'; ch <= 'z'; ch++)
-		putchar(ch);
-	for (ch = 'A'; ch <= 'Z'; ch++)
-		putchar(ch);
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a + '0');
+		if (a < 9) 
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
 	putchar('\n');
 	return (0);
 }
