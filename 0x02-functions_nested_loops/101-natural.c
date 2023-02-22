7edb7452570bafd04d49934dd0edd480f7bd014c
DavidJoseckDavidgithub <joseckonyinodavid@gmail.com>
2023-02-22 02:34:26 -0800

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *main - entry point
+ *Return: zero if success
+ */
+int main(void)
+{
+	int i;
+	int sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			sum += i;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
