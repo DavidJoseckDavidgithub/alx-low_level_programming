dbc8649b541ee0f5bc9231655944b07481008bb6
DavidJoseckDavidgithub <joseckonyinodavid@gmail.com>
2023-02-22 01:55:39 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - sums up two integers
+ * @j: first parameter
+ * @k: second parameter
+ * Return: always the result of summation
+ */
+int add(int j, int k)
+{
+	return (j + k);
+}
