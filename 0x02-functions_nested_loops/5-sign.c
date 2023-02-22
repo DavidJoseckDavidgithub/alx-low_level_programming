f83504d98aae7c021ac4562e41e52bc1121a0125
DavidJoseckDavidgithub <joseckonyinodavid@gmail.com>
2023-02-22 00:31:37 -0800

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *print_sign - prints the sign of a number
+ *Return: 1 if greater than zero else 0 or -1
+ *@n: integer argument
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(0 + '0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
