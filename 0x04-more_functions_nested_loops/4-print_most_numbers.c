cc6b3771cf66f0675e7ca86bb8f4cf8a9f334b0e
DavidJoseckDavidgithub <joseckonyinodavid@gmail.com>
2023-02-23 04:55:23 -0800

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,16 +3,21 @@
 /**
  * print_most_numbers - prints numbers 0 to 9 with exception of 2 and 4
  *
- * Return: void
+ * Return: The numbers 0 to 9
  */
 void print_most_numbers(void)
 {
 	int n = 0;
 
-	while (n <= 9)
+	for (; n <= 9; n++)
 	{
-		if (n != 2 && n != 4)
-		_putchar(n + '0');
+		if (n == 2 || n == 4)
+		{
+			continue;
+		}
+		{
+			_putchar(n + '0');
+		}
 	}
 	_putchar('\n');
 }
