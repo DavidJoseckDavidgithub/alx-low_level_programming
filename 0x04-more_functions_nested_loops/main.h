4cbf12621d2c7423b285795ff7443b49c1b67de7
DavidJoseckDavidgithub <joseckonyinodavid@gmail.com>
2023-02-23 03:06:53 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef main_h
+#define main_h
+
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -11,4 +14,4 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
-
+#endif
