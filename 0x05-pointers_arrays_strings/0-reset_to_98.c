8c94cfc9fad190136a5825eebe5bbaf26a8d4749
DavidJoseckDavidgithub <joseckonyinodavid@gmail.com>
2023-02-27 00:03:55 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value of n to 98
+ * @n: integer pointer to a variable to be updated
+ *
+ * Return: 0 (success)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,9 @@
+0x05. C - Pointers, arrays and strings
+
+Resources
+Read or watch:
+
+C - Arrays
+C - Pointers
+C - Strings
+Memory Layout
