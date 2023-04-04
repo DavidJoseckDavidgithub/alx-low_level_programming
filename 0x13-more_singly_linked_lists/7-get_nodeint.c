63a31e207b4fb450893bce833dfbe249915e92cb
DavidJoseckDavidgithub <joseckonyinodavid@gmail.com>
2023-04-04 14:36:00 -0700

    More lingly linked lists

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,13 +1,14 @@
 #include "lists.h"
 
 /**
- * get_nodeingt_at_index - returns the nth node of a linked list
- * @head: head of list
- * @index: index of the node
+ * get_nodeint_at_index - Locates a given node of
+ *                        a listint_t linked list.
+ * @head: A pointer to the head of the listint_t list.
+ * @index: The index of the node to locate - indices start at 0.
  *
- * Return: nth node. If node does not exist, returns NULL.
+ * Return: If the node does not exist - NULL.
+ *         Otherwise - the located node.
  */
-
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 unsigned int b;
