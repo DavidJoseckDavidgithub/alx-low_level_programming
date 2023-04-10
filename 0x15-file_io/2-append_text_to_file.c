66dfd20ffe8075656e1515bbb3793efa6d2d3dcc
DavidJoseckDavidgithub <joseckonyinodavid@gmail.com>
2023-04-10 09:19:28 -0700

    input/output

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,33 +8,32 @@
  * Return: 1 if the file exists. -1 if the fails does not exist
  * or if it fails.
  */
-
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
-int fd;
-int nletters;
-int rwr;
+	int fd;
+	int nletters;
+	int rwr;
 
-if (!filename)
-return (-1);
+	if (!filename)
+		return (-1);
 
-fd = open(filename, O_WRONLY | O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND);
 
-if (fd == -1)
-return (-1);
+	if (fd == -1)
+		return (-1);
 
-if (text_content)
-{
-for (nletters = 0; text_content[nletters]; nletters++)
-;
+	if (text_content)
+	{
+		for (nletters = 0; text_content[nletters]; nletters++)
+			;
 
-rwr = write(fd, text_content, nletters);
+		rwr = write(fd, text_content, nletters);
 
-if (rwr == -1)
-return (-1);
-}
+		if (rwr == -1)
+			return (-1);
+	}
 
-close(fd);
+	close(fd);
 
-return (1);
+	return (1);
 }
