495d8c22f16403e9efe01473e5fdcc68e2a56a5a
DavidJoseckDavidgithub <joseckonyinodavid@gmail.com>
2023-07-13 15:27:26 -0700

    more linkly list

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -62,7 +62,7 @@ int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 s1 = argv[1], s2 = argv[2];
 if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 errors();
-len1 = _strlen(s1;)
+len1 = _strlen(s1);
 len2 = _strlen(s2);
 len = len1 + len2 + 1;
 result = malloc(sizeof(int) * len);
@@ -77,7 +77,7 @@ carry = 0;
 for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 {
 digit2 = s2[len2] - '0';
-carry += result[len1 + len2 + 1] + (digit1 *digit2);
+carry += result[len1 + len2 + 1] + (digit1 * digit2);
 result[len1 + len2 + 1] = carry % 10;
 carry /= 10;
 }
