7940b324a78127dd6f148fd55d0ecdf0cbc1f031
DavidJoseckDavidgithub <joseckonyinodavid@gmail.com>
2023-07-13 15:30:39 -0700

    more linkly list

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -77,7 +77,7 @@ carry = 0;
 for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 {
 digit2 = s2[len2] - '0';
-carry += result[len1 + len2 + 1] + (digit1 * digit2);
+carry += result[len1 + len2 + 1] + (digit1 *digit2);
 result[len1 + len2 + 1] = carry % 10;
 carry /= 10;
 }
