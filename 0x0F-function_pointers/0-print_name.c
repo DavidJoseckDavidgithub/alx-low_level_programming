c8c875bd3c300ef7e1ac600b189613b272b514f4
DavidJoseckDavidgithub <joseckonyinodavid@gmail.com>
2023-07-19 15:19:48 -0700

    now

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -7,12 +7,8 @@
  *
  * Return: no return.
  */
-
-void print_name(char *name, void(*f)(char *))
+void print_name(char *name, void (*f)(char *))
 {
-	if (!name || !f)
-	{
-		return;
-	}
-	f(name);
+	if (name && f)
+		f(name);
 }
