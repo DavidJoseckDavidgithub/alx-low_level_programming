2d4b3feebf7858eeeea8aa18894d59c26e366176
Joseck David <joseckonyinodavid@gmail.com>
2024-01-10 08:06:05 -0800

    This function takes a pointer to a string as an argument and prints each character from the end to the beginning. The function calls itself on the next character in the string, using pointer arithmetic, until it reaches the null terminator. Then it prints the characters in reverse order as the stack unwinds

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,9 +7,10 @@
 
 void _print_rev_recursion(char *s)
 {
-	if (*s)
-	{
-		_print_rev_recursion(s + 1);
-		_putchar(*s);
-	}
+if (*s == '\0')
+{
+return;
+}
+_print_rev_recursion(s + 1);
+_putchar(*s);
 }
