c534f5a550036ea5032d08ded4e91d448fdbdcf7
Joseck David <joseckonyinodavid@gmail.com>
2024-01-10 00:57:15 -0800

    Start 2024

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,17 +2,18 @@
 /**
  * _puts_recursion - print a string
  * @s:string
+ * *s:pointer to astring
  * Return:void
  */
 void _puts_recursion(char *s)
 {
-if (*s)
-{
-	_putchar(*s);
-	_puts_recursion(s + 1);
-}
-else
+// base case: if the string is empty, print a new line and return
+if (*s == '\0')
 {
 	_putchar('\n');
+	return;
 }
+// recursive case: print the first line and call the function on the rest of the string
+_putchar(*s);
+_puts_recursion(s + 1);
 }
